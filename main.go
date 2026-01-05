a4b5912eb31f01e58cf459ff82c2e9314d790297
hiimchinh <chinhhm@proton.me>
2026-01-06 01:01:55 +0700

    handle repl basic

--- main.go
+++ main.go
@@ -7,17 +7,56 @@ import (
 	"strings"
 )
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Showing help",
+			callback:    commandHelp,
+		},
+	}
 	for {
 		fmt.Print("Pokedex > ")
 		for scanner.Scan() {
 			line := scanner.Text()
 			arr := strings.Fields(line)
 			firstWord := strings.ToLower(arr[0])
-			fmt.Printf("Your command was: %v\n", firstWord)
+			command, ok := commands[firstWord]
+			if !ok {
+				fmt.Println("Unknown command")
+				break
+			}
+			command.callback()
 			break
-
 		}
 
 	}
